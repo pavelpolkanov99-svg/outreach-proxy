f1f685085120b10623717972e6ba9d100bf27068
pavelpolkanov99-svg <pavelpolkanov99@gmail.com>
2026-04-16 22:44:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-# Запускаем прокси в фоне — это главный процесс
-node index.js &
-PROXY_PID=$!
-
-# Запускаем бота отдельно с автоперезапуском
-while true; do
-  node bot.js
-  echo "[start.sh] bot.js exited, restarting in 5s..."
-  sleep 5
-done &
-
-# Ждём прокси (главный процесс)
-wait $PROXY_PID
+node index.js
